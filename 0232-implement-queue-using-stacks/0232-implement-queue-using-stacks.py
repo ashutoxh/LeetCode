938f1ff275cb0d53b51d7e4734d2fb46a6682e4a
Ashutosh Singh <atsingh1997@gmail.com>
2023-10-25 18:18:23 -0400

    Time: 34 ms (78.83%), Space: 16.6 MB (19.36%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,129 @@
+class ListNode:
+    # NOTHING CAN BE CHANGED HERE
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+############################################################
+#  class  Slist
+###########################################################
+
+
+class Slist:
+    def __init__(self):
+        # NOTHING CAN BE CHANGED HERE
+        self._first = None
+        self._last = None
+        self._len = 0
+
+    #############################
+    # WRITE All public functions BELOW
+    # YOU CAN HAVE ANY NUMBER OF PRIVATE FUNCTIONS YOU WANT
+    #############################
+
+    def _add_first(self, value: int):
+        new_node = ListNode(value)
+
+        if self._first is None and self._last is None:
+            self._first = new_node
+            self._last = new_node
+        else:
+            new_node.next = self._first
+            self._first = new_node
+        self._len += 1
+
+    def _add_last(self, value: int):
+        new_node = ListNode(value)
+
+        if self._first is None and self._last is None:
+            self._first = new_node
+            self._last = new_node
+        else:
+            self._last.next = new_node
+            self._last = new_node
+        self._len += 1
+
+    def _delete_first(self):
+        if self._first is not None:
+            if self._last == self._first:
+                self._first = None
+                self._last = None
+                self._len -= 1
+                return
+            self._first = self._first.next
+            self._len -= 1
+            return True
+        return False
+
+    def _delete_last(self):
+        if self._first is not None:
+            temp = self._first
+            if self._last == self._first:
+                self._first = None
+                self._last = None
+                self._len -= 1
+                return
+            while temp.next.next is not None:
+                temp = temp.next
+            self._last = temp
+            self._last.next = None
+            self._len -= 1
+            return True
+        return False
+
+    def _get_first(self):
+        if self._first is not None:
+            return self._first.val
+
+    def _get_last(self):
+        if self._last is not None:
+            return self._last.val
+
+class MyQueue:
+    def __init__(self):
+        # NOTHING CAN BE CHANGED HERE
+        self._s = Slist()
+
+    def push(self, x: int) -> None:
+        self._s._add_last(x)
+
+    def pop(self) -> int:
+        top = self._s._get_first()
+        self._s._delete_first()
+        return top
+
+    def peek(self) -> int:
+        return self._s._get_first()
+
+    def empty(self) -> bool:
+        if self._s._len is 0:
+            return True
+        return False
+
+############################################################
+#  class  MyQueue
+# 232. Implement Queue using Stacks
+
+# https://leetcode.com/problems/implement-queue-using-stacks/
+########################################################### 
+class MyQueue:
+    def __init__(self):
+        # NOTHING CAN BE CHANGED HERE
+        self._s = Slist()
+
+    def push(self, x: int) -> None:
+        self._s._add_last(x)
+
+    def pop(self) -> int:
+        top = self._s._get_first()
+        self._s._delete_first()
+        return top
+
+    def peek(self) -> int:
+        return self._s._get_first()
+
+    def empty(self) -> bool:
+        if self._s._len is 0:
+            return True
+        return False
