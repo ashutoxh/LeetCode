c65aff1d299c9694d78eb07dd4b275447d922044
Ashutosh Singh <atsingh1997@gmail.com>
2023-11-18 12:38:58 -0500

    Time: 16 ms (10.76%), Space: 44.7 MB (5.21%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,70 +1,17 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean hasValidRows = checkRows(board);
-        // System.out.println("hasValidRows " + hasValidRows);
-        if(!hasValidRows)
-            return false;
-        boolean hasValidColumns = checkColumns(board);
-        // System.out.println("hasValidColumns " + hasValidColumns);
-        if(!hasValidColumns)
-            return false;
-        boolean hasValidGrid = checkGrid(board);
-        // System.out.println("hasValidGrid " + hasValidGrid);
-        if(!hasValidGrid)
-            return false;
-        return true;
-    }
-
-    private static boolean checkRows(char[][] board){
-        for(int row=0; row<board.length; row++){
-            Set<Character> unique = new HashSet<>();
-            for(int col=0; col<board[0].length; col++){
-                if(board[row][col] == '.')
-                    continue;
-                if(unique.contains(board[row][col])){
+    Set seen = new HashSet();
+    for (int i=0; i<9; ++i) {
+        for (int j=0; j<9; ++j) {
+            char number = board[i][j];
+            if (number != '.')
+                if (!seen.add(number + " in row " + i) ||
+                    !seen.add(number + " in column " + j) ||
+                    !seen.add(number + " in block " + i/3 + "-" + j/3))
                     return false;
-                } else{
-                    unique.add(board[row][col]);
-                }
-            }
         }
-        return true;
     }
-
-    private static boolean checkColumns(char[][] board){
-        for(int row=0; row<board.length; row++){
-            Set<Character> unique = new HashSet<>();
-            for(int col=0; col<board[0].length; col++){
-                if(board[col][row] == '.')
-                    continue;
-                if(unique.contains(board[col][row])){
-                    return false;
-                } else{
-                    unique.add(board[col][row]);
-                }
-            }
-        }
-        return true;
-    }
-
-    private static boolean checkGrid(char[][] board){
-        for (int gridRow = 0; gridRow < 9; gridRow += 3) {
-            for (int gridCol = 0; gridCol < 9; gridCol += 3) {
-                Set<Character> unique = new HashSet<>();
-                for (int row = gridRow; row < gridRow + 3; row++) {
-                    for (int col = gridCol; col < gridCol + 3; col++) {
-                        if(board[row][col] == '.')
-                            continue;
-                        if(unique.contains(board[row][col])){
-                            return false;
-                        } else{
-                            unique.add(board[row][col]);
-                        }
-                    }
-                }
-                // System.out.println(unique);
-            }
-        }
-        return true;
-    }
+    System.out.println(seen);
+    return true;
+    }   
 }
